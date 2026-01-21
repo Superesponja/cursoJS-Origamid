8073bdd61a5ae87b5e00c570e7f1b2f3b89ee71d
Renan <superesponja15@hotmail.com>
2026-01-20 23:07:32 -0300

    Prototype 3 completa, com exercício

--- Objetos/constructor-function/script.js
+++ Objetos/constructor-function/script.js
@@ -1,36 +1,48 @@
-// Crie 3 pessoas, João - 20 anos, Maria - 25 anos, Bruno - 15 anos
-
-function Pessoa(nome, idade){
+// Crie uma função construtora de Pessoas. Deve conter nome, sobrenome e idade
+function Pessoas(nome, sobrenome, idade){
   this.nome = nome;
-  this.idade = idade;
-  this.andar = function (){
-    console.log(this.nome + " andou");
-  }
+  this.sobrenome = sobrenome;
+  this.idade = idade;  
 }
-
-const joao = new Pessoa('João', 20);
-const maria = new Pessoa('Maria', 25);
-const bruno = new Pessoa('Bruno', 15);
-
-// Crie uma Constructor Function (Dom) para manipulação de listas de elementos do dom. Deve conter as seguintes propriedades e métodos:
-  // elements, retorna NodeList com os elementos selecionados
-  // addClass(classe), adiciona a classe a todos os elementos
-  // removeClass(classe), remove a classe a todos os elementos
-
-function Dom(seletor){  
-
-  this.element = document.querySelectorAll(seletor);
-
-  this.addClass = (classe) => {    
-    this.element.forEach((elemento) =>{
-      elemento.classList.add(classe);
-    });    
+// Crie um método no protótipo que retorne o nome completo da pessoa
+Pessoas.prototype.nomeCompleto = function(){
+    return `${this.nome} ${this.sobrenome}`;
   }
+// Liste os métodos acessados por dados criados com NodeList,
+// HTMLCollection, Document
+console.log("*****NODELIST*****");
+Object.getOwnPropertyNames(NodeList.prototype).forEach((prop) => {
+  console.log(prop);
+});
 
-  this.removeClasse = (classe) =>{
-    this.element.forEach((elemento) =>{
-      elemento.classList.remove(classe);
-    })
-  }
-}
-const umaUl = new Dom('ul');
+console.log("*****HTMLCollection*****");
+Object.getOwnPropertyNames(HTMLAllCollection.prototype).forEach((prop) => {
+  console.log(prop);
+});
+
+console.log("*****Document*****");
+Object.getOwnPropertyNames(Document.prototype).forEach((prop) => {
+  console.log(prop);
+});
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li;
+console.log(li.constructor.name);
+li.click;
+console.log(li.click.constructor.name);
+li.innerText;
+console.log(li.innerText.constructor.name);
+li.value;
+console.log(li.value.constructor.name);
+li.hidden;
+console.log(li.hidden.constructor.name);
+li.offsetLeft;
+console.log(li.offsetLeft.constructor.name);
+li.click();
+// quebrou o código console.log(li.click().constructor.name);
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
+//RESPOSTA: string
